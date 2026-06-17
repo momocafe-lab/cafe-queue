ba38b10fd7498688ed144f51087b4f5670191914
momocafe <info@momo-cafe.com>
2026-06-17 18:18:20 +0300

    add send-sms api

---
+++ api/send-sms.js
@@ -0,0 +1,23 @@
+const twilio = require('twilio')
+
+const client = twilio(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+)
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') return res.status(405).end()
+
+  const { to, message } = req.body
+
+  try {
+    await client.messages.create({
+      body: message,
+      from: process.env.TWILIO_PHONE_NUMBER,
+      to: to
+    })
+    res.status(200).json({ success: true })
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
