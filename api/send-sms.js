ac32c280ecb4b398ab410c7ec9dd998981ac694b
momocafe <info@momo-cafe.com>
2026-06-18 15:46:45 +0300

    debug: log twilio error

--- api/send-sms.js
+++ api/send-sms.js
@@ -9,6 +9,7 @@ export default async function handler(req, res) {
     await client.messages.create({ body: message, from: process.env.TWILIO_PHONE_NUMBER, to: to })
     res.status(200).json({ success: true })
   } catch (error) {
-    res.status(500).json({ error: error.message })
+   console.log('Twilio error:', error.message)
+  res.status(500).json({ error: error.message })
   }
 }
